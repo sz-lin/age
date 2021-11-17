0ed12afa1a5bef5d981fe8ba2e8e341fbbc16820
sz-lin <shiaw790709@gmail.com>
2021-11-17 14:12:25 +0800

    Add new file

---
+++ age.py
@@ -0,0 +1,21 @@
+#程式練習-年齡判斷
+driving = input('請問你有沒有開過車')
+if driving != '有' and driving != '沒有':
+   print('只能輸入 有/沒有')
+   raise SystemExit  #強制停止跳出程式
+
+age = input('請問你的年齡')
+age = int(age)
+if driving == '有':  #(兩個等於左右比對)
+	if age >= 18:
+		print('你通過測驗了')
+	else:
+		print('奇怪  你怎麼會有開過車')
+elif driving == '沒有':
+    if age >=18:
+    	print('你可以考駕照了，怎麼還不去考')
+    else:
+    	print('很好，再過幾年就可以考駕照了')
+#else:
+#	print('只能輸入 有/沒有')
+	
